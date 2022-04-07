e136e2709b4fd82a474d07530a9caf4ef68b1d0f
Timothy Hale <timothyh@tutanota.com>
2022-04-07 18:43:42 +0200

    more test covergae

--- src/main/scala/model/Field.scala
+++ src/main/scala/model/Field.scala
@@ -1,9 +1,9 @@
 package de.htwg.se.pokelite.model
 
 @main def run() : Unit =
-  println(screen)
+  println(mesh)
 
-def screen: String = row() + col() + row()
-def row  (width : Int = 60): String = "+"+("-"*width+"+")*2+"\n"
-def col (height: Int = 1): String = (("|"+" "*60)*2+"|\n")*height
+def mesh (width : Int =  60, height : Int = 60): String = row(width) + col(width, height) + row(width)
+def row  (width : Int = 50): String = "+"+("-"*width+"+")*2+"\n"
+def col (width : Int = 50, height: Int = 3): String = (("|"+" "*width)*2+"|\n")*height
 
--- src/test/scala/model/FieldSpec.scala
+++ src/test/scala/model/FieldSpec.scala
@@ -7,10 +7,26 @@ class FieldSpec extends AnyWordSpec {
   "A PokemonLite Field" when {
     "empty" should {
       "have a bar as String of form '+---+---+'" in {
-        row() should be("+------------------------------------------------------------+------------------------------------------------------------+\n")
+        row(10) should be("+----------+----------+\n")
       }
       "have a cell as String of form '|  |  |'" in {
-        col() should be("|                                                            |                                                            |\n")
+        col(10, 1) should be("|          |          |\n")
+      }
+      "have a scalable cell" in {
+        col(9, 1) should be("|         |         |\n")
+        col(11, 1) should be("|           |           |\n")
+        col(10, 2) should be("|          |          |\n"*2)
+      }
+      "have a scalable bar" in {
+        row(1) should be("+-+-+\n")
+        row(2) should be("+--+--+\n")
+        row(3) should be("+---+---+\n")
+      }
+      "have a mesh in the form " +
+        "+-+-+" +
+        "| | |" +
+        "+-+-+" in {
+        mesh(1, 1) should be("+-+-+\n" + "| | |\n" + "+-+-+\n")
       }
     }
   }
