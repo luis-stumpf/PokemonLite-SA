caedaf9b5c7eead530fd3dfebcd4bf51f9dafe3a
Timothy Hale <timothyh@tutanota.com>
2022-04-07 18:57:44 +0200

    removed default values

--- src/main/scala/model/Field.scala
+++ src/main/scala/model/Field.scala
@@ -1,9 +1,9 @@
 package de.htwg.se.pokelite.model
 
 @main def run() : Unit =
-  println(mesh)
+  println(mesh(60, 60))
 
-def mesh (width : Int =  60, height : Int = 60): String = row(width) + col(width, height) + row(width)
-def row  (width : Int = 50): String = "+"+("-"*width+"+")*2+"\n"
-def col (width : Int = 50, height: Int = 3): String = (("|"+" "*width)*2+"|\n")*height
+def mesh (width : Int, height : Int): String = row(width) + col(width, height) + row(width)
+def row  (width : Int): String = "+"+("-"*width+"+")*2+"\n"
+def col (width : Int, height: Int): String = (("|"+" "*width)*2+"|\n")*height
 
