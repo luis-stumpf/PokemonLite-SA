64813e08d1af0e6b24cf744196d79fd646fa7307
Timothy Hale <timothyh@tutanota.com>
2022-04-19 15:14:37 +0200

    cleaned up messy shit

--- src/main/scala/model/Pokemon.scala
+++ src/main/scala/model/Pokemon.scala
@@ -2,12 +2,12 @@ package de.htwg.se.pokelite
 package model
 
 enum Pokemon(stringRepresentation: String, hp: Int):
-  override def toString = stringRepresentation;
-  def getHp = hp;
-  case G extends Pokemon("G", 110);
-  case P extends Pokemon("P", 120);
-  case B extends Pokemon("B", 130);
-  case M extends Pokemon("M", 140);
-  case R extends Pokemon("R", 150);
-  case S extends Pokemon("S", 160);
-  case Empty extends Pokemon("No Pokemon", 10000);
+  override def toString: String = stringRepresentation
+  def getHp: Int = hp
+  case G extends Pokemon("G", 110)
+  case P extends Pokemon("P", 120)
+  case B extends Pokemon("B", 130)
+  case M extends Pokemon("M", 140)
+  case R extends Pokemon("R", 150)
+  case S extends Pokemon("S", 160)
+  case Empty extends Pokemon("No Pokemon", 10000)
