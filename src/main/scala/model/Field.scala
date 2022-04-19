10168b5fd0f248c37471cf5d2745ae1c30d43a7a
Timothy Hale <timothyh@tutanota.com>
2022-04-19 15:41:37 +0200

    input commited

--- src/main/scala/model/Field.scala
+++ src/main/scala/model/Field.scala
@@ -2,11 +2,14 @@ package de.htwg.se.pokelite.model
 import scala.io.StdIn.readLine
 
 @main def run() : Unit =
+  println(mesh(60, 15, " ", " "))
   println("Enter name of Player 1: ")
   val namePlayer1 = readLine()
-  println("Enter name of Player 42: ")
+  println(mesh(60, 15, namePlayer1, " "))
+  println("Enter name of Player 2: ")
   val namePlayer2 = readLine()
   println(mesh(60, 15, namePlayer1, namePlayer2))
+
 
 def mesh (width : Int, height : Int, nP1 : String, nP2 : String): String = row(width) + printPlayer1(width, nP1) + col(width, height) + printPlayer2(width, nP2) + row(width)
 def row  (width : Int): String = "+"+("-"*width+"+")*2+"\n"
