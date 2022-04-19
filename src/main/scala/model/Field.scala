c7e08fc59be38e506b50d50171e8e01c101deee3
Timothy Hale <timothyh@tutanota.com>
2022-04-19 15:36:59 +0200

    cleaned up messy shit

--- src/main/scala/model/Field.scala
+++ src/main/scala/model/Field.scala
@@ -4,9 +4,11 @@ import scala.io.StdIn.readLine
 @main def run() : Unit =
   println("Enter name of Player 1: ")
   val namePlayer1 = readLine()
-  println("Enter name of Player 42: ")
-  val namePlayer2 = readLine()
   println(mesh(60, 15))
+  println("Enter name of Player 2d: ")
+  val namePlayer2 = readLine()
+
+
 
 def mesh (width : Int, height : Int): String = row(width) + col(width, height) + row(width)
 def row  (width : Int): String = "+"+("-"*width+"+")*2+"\n"
