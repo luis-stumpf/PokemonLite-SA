0c3cab86418e30e5f4b9e80544dbeaed58ee47b6
luis-stumpf <trumpf25lp@gmail.com>
2022-04-19 14:06:03 +0200

    merged firstTest into development

---
+++ src/main/scala/model/Pokemon.scala
@@ -0,0 +1,13 @@
+package de.htwg.se.pokelite
+package model
+
+enum Pokemon(stringRepresentation: String, hp: Int):
+  override def toString = stringRepresentation;
+  def getHp = hp;
+  case G extends Pokemon("G", 110);
+  case P extends Pokemon("P", 120);
+  case B extends Pokemon("B", 130);
+  case M extends Pokemon("M", 140);
+  case R extends Pokemon("R", 150);
+  case S extends Pokemon("S", 160);
+  case Empty extends Pokemon("No Pokemon");
