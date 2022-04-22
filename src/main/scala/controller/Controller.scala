1d18bf60365aa166539b4c5d09f7343b9c0ef147
luis-stumpf <trumpf25lp@gmail.com>
2022-04-22 13:54:31 +0200

    tried building new main

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -1,6 +1,8 @@
 package de.htwg.se.pokelite
 package controller
 
-class Controller {
+import util.Observable
+import model.Field
 
-}
+case class Controller(var field: Field) extends Observable:
+  override def toString = field.toString;
--- src/main/scala/model/Field.scala
+++ src/main/scala/model/Field.scala
@@ -1,7 +1,7 @@
 package de.htwg.se.pokelite.model
 
 case class Field(width: Int):
-  def mesh (width : Int, height : Int, nP1 : String, nP2 : String): String = row(width) + printPlayer1(width, nP1) + printPokemonP1(width, Pokemon("Glurak", 150)) + col(width, height) + printPokemonP2(width, Pokemon("Simsala", 200)) + printPlayer2(width, nP2) + row(width)
+  def mesh (width : Int = 50, height : Int = 3, nP1 : String = "Luis", nP2 : String = "Timmy"): String = row(width) + printPlayer1(width, nP1) + printPokemonP1(width, Pokemon("Glurak", 150)) + col(width, height) + printPokemonP2(width, Pokemon("Simsala", 200)) + printPlayer2(width, nP2) + row(width)
   def row  (width : Int): String = "+"+("-"*width+"+")*2+"\n"
   def col (width : Int, height: Int): String = (("|"+" "*width)*2+"|\n")*height
   def printPlayer1 (width: Int, name: String): String = printP1left(width, name) + cleanSite(width)
@@ -16,3 +16,5 @@ case class Field(width: Int):
   def printP2left (width: Int, name: String): String = "|"+" "*calcSpace(width, 0.1) + name + " " * calcSpace(width, 0.9, name)
   def printPokeP1left (width: Int, pokemon: Pokemon): String = "|"+" "*calcSpace(width, 0.9, pokemon.toString) + pokemon + " "* calcSpace(width, 0.1)
   def printPokeP2right (width: Int, pokemon: Pokemon): String = "|"+" "* calcSpace(width, 0.1) + pokemon + " "* calcSpace(width, 0.9, pokemon.toString)
+
+  override def toString = mesh()
