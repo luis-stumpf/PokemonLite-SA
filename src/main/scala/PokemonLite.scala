ea825bdfb757dda905afb2916fe6dfaf4694f402
luis-stumpf <trumpf25lp@gmail.com>
2022-04-22 13:33:09 +0200

    tried building new main

--- src/main/scala/PokemonLite.scala
+++ src/main/scala/PokemonLite.scala
@@ -1,5 +1,16 @@
 package de.htwg.se.pokelite
 
-class PokemonLite {
+import de.htwg.se.pokelite.model.Field
 
-}
+import scala.io.StdIn.readLine
+
+
+@main def run() : Unit =
+  val field = new Field(3)
+  println(field.mesh(60, 3, " ", " "))
+  println("Enter name of Player 1: ")
+  val namePlayer1 = readLine()
+  println(field.mesh(60, 3, namePlayer1, " "))
+  println("Enter name of Player 2: ")
+  val namePlayer2 = readLine()
+  println(field.mesh(60, 3, namePlayer1, namePlayer2))
