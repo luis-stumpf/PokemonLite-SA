2cc807024c031a0327a3edc474c028e24ba7179c
luis-stumpf <trumpf25lp@gmail.com>
2022-04-22 14:10:13 +0200

    Tui bissle

--- src/main/scala/aview/TUI.scala
+++ src/main/scala/aview/TUI.scala
@@ -1,6 +1,12 @@
 package de.htwg.se.pokelite
 package aview
 
-class TUI {
+import controller.Controller
+import util.Observer
 
-}
+
+class TUI(controller: Controller) extends Observer:
+  controller.add(this)
+
+  override def update = ???
+
