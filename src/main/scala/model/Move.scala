05c676e72e64ad83230fcadab70901a6b205a5ac
luis-stumpf <trumpf25lp@gmail.com>
2022-04-29 11:06:28 +0200

    implemented do and publish

--- src/main/scala/model/Move.scala
+++ src/main/scala/model/Move.scala
@@ -2,24 +2,3 @@ package de.htwg.se.pokelite
 package model
 
 case class Move(name: String = "", pokemon: PokemonType = NoPokemon())
-
-
-/*
-trait Move{
-  val name: String
-  val pokemon: PokemonType
-}
-
-
-case class StrMove(name: String) extends Move{
-  
-}
-
-case class PokeMove(pokemon: PokemonType) extends Move{
-  
-}
-
-case class NoMove() extends Move{
-  
-}
-*/
