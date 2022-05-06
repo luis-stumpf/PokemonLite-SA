719e9ef1bfbb1a37931eafa61678a84c3aa5de3f
luis-stumpf <trumpf25lp@gmail.com>
2022-05-06 16:09:13 +0200

    passt so

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -9,6 +9,4 @@ case class Controller(var field: Field) extends Observable:
   def doAndPublish(doThis: Move => Field = put, move: Move) =
     field = doThis(move)
     notifyObservers
-  def onlyDo(doThis: Move => Field = put, move: Move) =
-    field = doThis(move)
   def put(move: Move): Field = move.doStep(field)
