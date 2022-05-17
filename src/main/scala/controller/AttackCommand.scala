ee67ffd97821ec03ba00ab185f854079c1a1f9a0
luis-stumpf <trumpf25lp@gmail.com>
2022-05-17 17:38:16 +0200

    maybe

--- src/main/scala/controller/AttackCommand.scala
+++ src/main/scala/controller/AttackCommand.scala
@@ -9,4 +9,4 @@ class AttackCommand(move: AttackMove) extends Command[Field]:
   override def noStep(field: Field): Field = field
   override def doStep(field: Field): Field = field.attack(move.attack).setNextTurn()
   override def undoStep(field: Field): Field = field.setNextTurn().attackInv(move.attack)
-  override def redoStep(field: Field): Field = field.attack(move.attack)
+  override def redoStep(field: Field): Field = field.attack(move.attack).setNextTurn()
