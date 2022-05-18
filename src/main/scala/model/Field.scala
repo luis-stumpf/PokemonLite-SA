730d1177c97f80ebb2eebf851f031b210b432c16
luis-stumpf <trumpf25lp@gmail.com>
2022-05-18 14:09:23 +0200

    strat is drin

--- src/main/scala/model/Field.scala
+++ src/main/scala/model/Field.scala
@@ -50,14 +50,7 @@ case class Field(width : Int, player1 : PokePlayer, player2 : PokePlayer, isCont
 
   def setNextTurn() : Field = if ( isControlledBy == 1 ) copy( isControlledBy = 2 ) else copy( isControlledBy = 1 )
 
-  def attack(attack : Int) : Field = if ( isControlledBy == 1 )
-    var mult = calcDamage( player1.pokemons.apply( player1.currentPoke ).get.pType.pokemonArt, player2.pokemons.apply( player2.currentPoke ).get.pType.pokemonArt )
-    copy(
-      player2 = player2.copy( pokemons = player2.pokemons.updated( player2.currentPoke, Some( player2.pokemons.apply( player2.currentPoke ).get.changeHp( player1.pokemons.apply( player1.currentPoke ).get.pType.attacks.apply( attack ), mult ) ) ) ) )
-  else
-    var mult = calcDamage( player2.pokemons.apply( player2.currentPoke ).get.pType.pokemonArt, player1.pokemons.apply( player1.currentPoke ).get.pType.pokemonArt )
-    copy(
-      player1 = player1.copy( pokemons = player1.pokemons.updated( player1.currentPoke, Some( player1.pokemons.apply( player1.currentPoke ).get.changeHp( player2.pokemons.apply( player2.currentPoke ).get.pType.attacks.apply( attack ), mult ) ) ) ) )
+  def attack(attack : Int) : Field = AttackPlayerStrat.strategy(attack)
 
   def attackInv(attack : Int) : Field = if ( isControlledBy == 1 ) copy(
     player2 = player2.copy( pokemons = player2.pokemons.updated( player2.currentPoke, Some( player2.pokemons.apply( player2.currentPoke ).get.changeHpInv( player2.pokemons.apply( player2.currentPoke ).get.pType.attacks.apply( attack ) ) ) ) ) )
@@ -88,4 +81,20 @@ case class Field(width : Int, player1 : PokePlayer, player2 : PokePlayer, isCont
 
   override def toString : String = mesh()
 
-  
+
+  object AttackPlayerStrat {
+
+    var strategy = if (isControlledBy == 1) strategy1 else strategy2
+
+    def strategy1(attack: Int) =
+      var mult = calcDamage( player1.pokemons.apply( player1.currentPoke ).get.pType.pokemonArt, player2.pokemons.apply( player2.currentPoke ).get.pType.pokemonArt )
+      copy(
+        player2 = player2.copy( pokemons = player2.pokemons.updated( player2.currentPoke, Some( player2.pokemons.apply( player2.currentPoke ).get.changeHp( player1.pokemons.apply( player1.currentPoke ).get.pType.attacks.apply( attack ), mult ) ) ) ) )
+
+
+    def strategy2(attack: Int) =
+      var mult = calcDamage( player2.pokemons.apply( player2.currentPoke ).get.pType.pokemonArt, player1.pokemons.apply( player1.currentPoke ).get.pType.pokemonArt )
+      copy(
+        player1 = player1.copy( pokemons = player1.pokemons.updated( player1.currentPoke, Some( player1.pokemons.apply( player1.currentPoke ).get.changeHp( player2.pokemons.apply( player2.currentPoke ).get.pType.attacks.apply( attack ), mult ) ) ) ) )
+
+  }
