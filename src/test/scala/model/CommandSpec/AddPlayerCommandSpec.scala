6d3ad70fed33a466c9cca9d9e52fec385a1f971c
Luis Stumpf <trumpf25lp@gmail.com>
2022-06-02 23:57:44 +0200

    passt fast

--- src/test/scala/model/CommandSpec/AddPlayerCommandSpec.scala
+++ src/test/scala/model/CommandSpec/AddPlayerCommandSpec.scala
@@ -1,18 +1,24 @@
 package de.htwg.se.pokelite
 package model.CommandSpec
 import de.htwg.se.pokelite.model.impl.game.Game
-import de.htwg.se.pokelite.model.states.InitPlayerState
+import de.htwg.se.pokelite.model.states.{InitPlayerPokemonState, InitPlayerState}
 import model.commands.AddPlayerCommand
+
+import de.htwg.se.pokelite.model.NoInput
+import de.htwg.se.pokelite.model.impl.pokePlayer.PokePlayer
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
+import scala.util.Try
 
 import scala.util.Success
 
 class AddPlayerCommandSpec extends AnyWordSpec {
   "AddPlayerCommand" when {
     val newGame:Game = Game()
+    val newGameWithPlayer = Game(player2 = Some(PokePlayer("name")))
     "doStep" should {
       val state = InitPlayerState()
+      val state1 = InitPlayerPokemonState()
       "success" in {
         val command = AddPlayerCommand("Luis", state)
         val res = command.doStep(newGame)
@@ -26,7 +32,14 @@ class AddPlayerCommandSpec extends AnyWordSpec {
           "|                                                  |                                                  |\n" +
           "|                                                  |                                                  |\n" +
           "+--------------------------------------------------+--------------------------------------------------+\n")
+        val res1 = command.doStep(res.get)
+        res1.get.state should be(InitPlayerPokemonState())
 
+      }
+      "failure" in {
+        val command = AddPlayerCommand("", state = state)
+        val res = command.doStep(newGame)
+        res.isFailure should be(true)
       }
     }
     "undo Step" should{
@@ -45,6 +58,20 @@ class AddPlayerCommandSpec extends AnyWordSpec {
           "|                                                  |                                                  |\n" +
           "|                                                  |                                                  |\n" +
           "+--------------------------------------------------+--------------------------------------------------+\n")
+        val res2 = command.doStep(res.get)
+        val res3 = command.undoStep(res2.get)
+        res3.toString should be("" +
+          "+--------------------------------------------------+--------------------------------------------------+\n" +
+          "|                                         Luis     |                                                  |\n" +
+          "|                                                  |                                                  |\n" +
+          "|                                                  |                                                  |\n" +
+          "|                                                  |                                                  |\n" +
+          "|                                                  |                                                  |\n" +
+          "|                                                  |                                                  |\n" +
+          "|                                                  |                                                  |\n" +
+          "+--------------------------------------------------+--------------------------------------------------+\n")
+
+
 
       }
 
