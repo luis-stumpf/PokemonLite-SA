1a54fe145f45ba3614957c9d6aa6ffc194ab16bc
Luis Stumpf <trumpf25lp@gmail.com>
2022-06-09 15:03:46 +0200

    di experiment

--- src/main/scala/PokemonLiteModule.scala
+++ src/main/scala/PokemonLiteModule.scala
@@ -3,7 +3,8 @@ package de.htwg.se.pokelite
 import com.google.inject.AbstractModule
 import controller.ControllerInterface
 import controller.impl.Controller
-import model.PokePlayerInterface
+import model.{FieldInterface, PokePlayerInterface}
+import model.impl.field.Field
 import model.impl.pokePlayer.PokePlayer
 
 
@@ -12,5 +13,6 @@ class PokemonLiteModule extends AbstractModule{
   override def configure(): Unit =
     bind(classOf[ControllerInterface]).to(classOf[Controller])
     bind(classOf[PokePlayerInterface]).to(classOf[PokePlayer])
+    bind(classOf[FieldInterface]).to(classOf[Field])
 
 }
