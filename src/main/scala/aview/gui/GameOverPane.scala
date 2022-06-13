ce40d2db466d005746fbc66b978ca54d64c82b66
Luis Stumpf <trumpf25lp@gmail.com>
2022-06-13 17:27:35 +0200

    added game over

--- src/main/scala/aview/gui/GameOverPane.scala
+++ src/main/scala/aview/gui/GameOverPane.scala
@@ -2,6 +2,8 @@ package de.htwg.se.pokelite
 package aview.gui
 
 import de.htwg.se.pokelite.controller.impl.Controller
+import scalafx.scene.text.Font
+import scalafx.scene.control.Label
 import scalafx.geometry.Pos
 import scalafx.scene.image.ImageView
 import scalafx.scene.image.Image
@@ -12,12 +14,17 @@ case class GameOverPane(controller: Controller) extends VBox {
   spacing = 30
   alignment = Pos.Center
 
+  val text = new Label("GAMEOVER!!!"){
+    font = Font.font(20)
+  }
+
   val logo = new Image("/Logo.png", 300, 300, true, true)
   val logoView = new ImageView(logo)
 
   children = List(
 
     logoView,
+    text,
     new Button("Play again!"){
       minWidth = 200
       minHeight = 60
