7d16ade85ce93f588bfc724e63f617ed02b77426
Luis Stumpf <trumpf25lp@gmail.com>
2022-06-24 11:29:05 +0200

    gui buttons

--- src/main/scala/aview/gui/DesicionPane.scala
+++ src/main/scala/aview/gui/DesicionPane.scala
@@ -8,18 +8,18 @@ import scalafx.scene.layout.HBox
 import scalafx.scene.control.Button
 
 
-case class DesicionPane(controller: ControllerInterface) extends HBox {
+case class DesicionPane( controller : ControllerInterface ) extends HBox {
   spacing = 50
   children = List(
-    new Button("Fight"){
+    new Button( "Fight" ) {
       minWidth = 200
       minHeight = 60
-      onAction = _ => controller.nextMove("1")
+      onAction = _ => controller.nextMove( "1" )
     },
-    new Button("Switch Pokemon"){
+    new Button( "Switch Pokemon" ) {
       minWidth = 200
       minHeight = 60
-      onAction = _ => controller.nextMove("2")
+      onAction = _ => controller.nextMove( "2" )
     },
   )
 }
