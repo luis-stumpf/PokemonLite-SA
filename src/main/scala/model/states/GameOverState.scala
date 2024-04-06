69524b98e48fa25eb29339389f52c69750ce9da0
Luis Stumpf <luis-stumpf@htwg-konstanz.de>
2024-04-06 22:47:03 +0200

    chore: new Command publish system

---
+++ src/main/scala/model/states/GameOverState.scala
@@ -0,0 +1,13 @@
+package de.htwg.se.pokelite
+package model.states
+
+import model.commands.{ ChangeStateCommand, GameOverCommand }
+import model.{ Command, GameInterface, State }
+
+case class GameOverState() extends State {
+  /*
+  override def restartTheGame( game : GameInterface ) : Option[ Command ] =
+    Some( GameOverCommand( game, this ) )
+
+   */
+}
---
+++ src/main/scala/model/states/InitState.scala
@@ -0,0 +1,19 @@
+package de.htwg.se.pokelite
+package model.states
+
+import model.*
+import model.commands.*
+import util.*
+
+case class InitState() extends State {
+  /*
+  override def initPlayers( ) : Option[ Command ] = Some(
+    ChangeStateCommand( this, InitPlayerState() )
+  )
+
+
+  override def restartTheGame(game: GameInterface): Option[Command] =
+    Some(GameOverCommand(game, this))
+
+   */
+}
